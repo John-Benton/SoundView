dcee19fba3292f5a0ed2bb08ae176a916b946e84
Unknown <shvproductions@yahoo.com>
2019-02-21 22:34:19 -0800

    Rendering with OpenGL and NanoVG is working

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -1,92 +1,201 @@
-/*
-  ==============================================================================
-
-    This file was auto-generated!
-
-  ==============================================================================
-*/
-
 #pragma once
+
+#include <glad/glad.h>
+
+#include <GLFW/glfw3.h>
+
+#define NANOVG_GL3_IMPLEMENTATION
+
+#include <nanovg.h>
+#include <nanovg_gl.h>
 
 #include "../JuceLibraryCode/JuceHeader.h"
 
-//==============================================================================
-/*
-    This component lives inside our window, and this is where you should put all
-    your controls and content.
-*/
-class MainComponent   : public AudioAppComponent
+class MainComponent   : public AudioAppComponent, public Button::Listener, public Timer
 {
 public:
-    //==============================================================================
+
     MainComponent()
     {
-        // Make sure you set the size of the component after
-        // you add any child components.
-        setSize (800, 600);
+		juce::Rectangle<int> screen = Desktop::getInstance().getDisplays().getMainDisplay().userArea; //Thank you Matthias Gehrmann
+	    setSize (screen.getWidth()*0.20, screen.getWidth()*0.5);
 
-        // specify the number of input and output channels that we want to open
+		addAndMakeVisible(display_window_visible_button);
+		display_window_visible_button.setButtonText("SHOW DISPLAY WINDOW");
+		display_window_visible_button.setColour(display_window_visible_button.buttonColourId, Colours::transparentBlack);
+
         setAudioChannels (2, 2);
-    }
+
+		setup_GL(screen.getWidth());
+
+		startTimerHz(30);
+
+    } 
 
     ~MainComponent()
     {
-        // This shuts down the audio device and clears the audio source.
         shutdownAudio();
+		glfwTerminate();
     }
 
-    //==============================================================================
     void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override
     {
-        // This function will be called when the audio device is started, or when
-        // its settings (i.e. sample rate, block size, etc) are changed.
 
-        // You can use this function to initialise any resources you might need,
-        // but be careful - it will be called on the audio thread, not the GUI thread.
-
-        // For more details, see the help for AudioProcessor::prepareToPlay()
     }
 
     void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override
     {
-        // Your audio-processing code goes here!
 
-        // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-        // Right now we are not producing any data, in which case we need to clear the buffer
-        // (to prevent the output of random noise)
         bufferToFill.clearActiveBufferRegion();
     }
 
     void releaseResources() override
     {
-        // This will be called when the audio device stops, or when it is being
-        // restarted due to a setting change.
 
-        // For more details, see the help for AudioProcessor::releaseResources()
     }
 
-    //==============================================================================
     void paint (Graphics& g) override
     {
-        // (Our component is opaque, so we must completely fill the background with a solid colour)
-        g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
 
-        // You can add your drawing code here!
+        g.fillAll (Colour(20,20,20));
+
     }
 
     void resized() override
     {
-        // This is called when the MainContentComponent is resized.
-        // If you add any child components, this is where you should
-        // update their positions.
+		control_window_outline = getLocalBounds();
+		int control_window_height = control_window_outline.getHeight();
+
+		display_window_visible_button_outline = control_window_outline.removeFromTop(control_window_height*0.05);
+		display_window_visible_button.setBounds(display_window_visible_button_outline.reduced(3));
     }
 
-
 private:
-    //==============================================================================
-    // Your private member variables go here...
 
+	juce::Rectangle<int> control_window_outline;
+	juce::Rectangle<int> display_window_visible_button_outline;
+
+	TextButton display_window_visible_button;
+
+	GLFWwindow *display_window;
+	struct NVGcontext *nvg_context;
+
+	int display_window_width, display_window_height;
+
+	void timerCallback() override
+	{
+
+		render_display();
+
+	}
+
+	void buttonClicked(Button* button) override 
+	{
+		
+	}
+
+	void setup_GL(int screen_width) {
+
+		glfwInit();
+
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		display_window = glfwCreateWindow(screen_width*0.5, screen_width*0.5, "SoundView Display", NULL, NULL);
+
+		if (display_window == NULL) {
+
+			AlertWindow::showMessageBox(AlertWindow::AlertIconType::WarningIcon, "ERROR", "GLFW could not set up a window", "OK", NULL);
+
+		}
+
+		glfwMakeContextCurrent(display_window);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+
+			AlertWindow::showMessageBox(AlertWindow::AlertIconType::WarningIcon, "ERROR", "GLAD could not be initialized", "OK", NULL);
+
+		}
+
+		nvg_context = nvgCreateGL3(NVG_ANTIALIAS | NVG_STENCIL_STROKES);
+
+		nvgCreateFont(nvg_context, "Arial", "C://Windows//Fonts//arial.ttf");
+		
+	}
+
+	void render_display() {
+
+		if (glfwWindowShouldClose(display_window))
+		{
+			JUCEApplicationBase::quit();
+		}
+
+		glClearColor(1.0, 1.0, 1.0, 1.0);
+
+		glfwGetWindowSize(display_window, &display_window_width, &display_window_height);
+
+		glViewport(0, 0, display_window_width, display_window_height);
+
+		nvg_render(nvg_context);
+
+		glfwSwapBuffers(display_window);
+
+	}
+
+	void nvg_render(NVGcontext *ctx)
+	{
+		nvgBeginFrame(ctx, display_window_width, display_window_height, 1.0f);
+
+		//==========//
+
+		nvgStrokeWidth(ctx, 2);
+
+		nvgStrokeColor(ctx, nvgRGBA(255, 255, 255, 255));
+
+		nvgBeginPath(ctx);
+
+		nvgRect(ctx, 0, 0, 100, 100);
+						
+		nvgStroke(ctx);
+
+		render_text(ctx, "TEST", 50, 50, 20);
+
+		//==========//
+
+		nvgEndFrame(ctx);
+
+	}
+
+	void render_text(NVGcontext *ctx, const char* text, int center_x_pix, int center_y_pix, int font_size_pix) {
+
+		int font_x, font_y;
+
+		float font_bounds[4];
+
+		nvgFontSize(ctx, font_size_pix);
+
+		nvgTextAlign(ctx, NVGalign::NVG_ALIGN_BOTTOM);
+
+		nvgTextBounds(ctx, 0.0, 0.0, text, NULL, font_bounds);
+
+		int font_width = font_bounds[2] - font_bounds[0];
+
+		int font_height = font_bounds[3] - font_bounds[1];
+
+		font_x = center_x_pix - (font_width / 2);
+
+		font_y = center_y_pix + (font_height / 2);
+
+		nvgText(ctx, font_x, font_y, text, NULL);
+
+		//nvgBeginPath(ctx);
+
+		//nvgRect(ctx, font_x, font_y - font_height, font_width, font_height);
+
+		//nvgStroke(ctx);
+
+	}
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
 };
